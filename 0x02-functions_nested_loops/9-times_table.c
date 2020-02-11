45439cf1ed9a393a130c00c85fb7d210e13bec06
julianfrancor <julianfrancor@gmail.com>
2020-02-11 21:17:32 +0000

    function that prints the 9 times table, starting with 0.

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,8 +1,7 @@
 #include "holberton.h"
 /**
- * jack_bauer - function that prints every
- * minute of the day of Jack Bauer
- * starting from 00:00 to 23:59.
+ * times_table - function that prints the 9
+ * times table, starting with 0
  * Return: void
  */
 
