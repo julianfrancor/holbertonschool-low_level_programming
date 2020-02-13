64696ba50db3e8ca4c0dd60a9d2b688e6d086958
julianfrancor <julianfrancor@gmail.com>
2020-02-13 23:02:43 +0000

    the Fizz-Buzz test

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+
 	for (i = 1; i <= 100; i++)
 	{
 		if (((i % 3) == 0) && ((i % 5) == 0))
@@ -25,7 +25,7 @@ int main(void)
 		}
 		else
 		{
-			printf("%d",i);
+			printf("%d", i);
 		}
 		printf(" ");
 	}
