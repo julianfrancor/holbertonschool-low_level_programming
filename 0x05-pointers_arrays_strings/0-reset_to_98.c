6d6130ba57d36b148fd52c39f2faf65b394434fa
julianfrancor <julianfrancor@gmail.com>
2020-02-18 16:10:39 +0000

    function that takes a pointer to an int as parameter and updates the value it points to to 98.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+
+/**
+   * print_numbers - function that prints the
+    * numbers, from 0 to 9, followed by a new line.
+     * input is void
+      * Return: void
+       */
+
+void reset_to_98(int *n)
+{;
+	*n = 98;
+}
