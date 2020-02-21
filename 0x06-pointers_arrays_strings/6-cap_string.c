63e628402415128e39fa612fd359611447d352ab
julianfrancor <julianfrancor@gmail.com>
2020-02-21 01:07:01 +0000

    dandole al 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * *cap_string - function that capitalizes
+ * all words of a string.
+ * @s: input is array
+ * Return: char
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+	char *sep;
+
+	i = 0;
+	sep[] = {32, 59, 46, 33, ?, ", (, ), ;
+
+
+	while (s[i] != 0)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z' && s[i] != )
+		{
+			s[i] = s[i] - 32;
+		}
+	i++;
+	}
+	return (s);
+}
