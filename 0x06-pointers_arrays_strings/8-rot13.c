f0e419131425386e228619890c521c1e8fc26379
julianfrancor <julianfrancor@gmail.com>
2020-02-21 22:32:30 +0000

    function that encodes a string using rot13.

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,8 +1,8 @@
 #include "holberton.h"
 
 /**
- * *leet - function that encodes a
- * string into 1337.
+ * *rot13 - function that encodes a
+ * string using rot13.
  * @s: input is array
  * Return: char
  */
@@ -13,13 +13,31 @@ char *rot13(char *s)
 	char a2[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
 	int j, i = 0;
 
+	/*with this while we travel along the input array*/
 	while (s[i] != 0)
 	{
+	/*
+	 * with this for we gonna compare the content of
+	 * the array s and a1
+	 */
 		for (j = 0; a1[j] != 0; j++)
 		{
+			/*
+			 * with this if we gonna over-write the
+			 * content of the array s
+			 */
 			if (s[i] == a1[j])
 			{
 				s[i] = a2[j];
+			/*
+			 * the break has to be inside the if
+			 * because if the program finds a character
+			 * in s that is equal in a1 it will change it
+			 * with a value of array a2 and then will break
+			 * the for, if we place this break out of the for
+			 * we gonna stop that for always and we'll end-up not
+			 * doing anything.
+			 */
 				break;
 			}
 
