630ac3b5f34759132e867638237015663ac07663
julianfrancor <julianfrancor@gmail.com>
2020-03-01 01:12:28 +0000

    Print number exercise

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -1,30 +1,26 @@
 #include "holberton.h"
 
 /**
- * print_number - function that
- * prints an integer.
- * @n: input is an intiger
- * Return: void
- */
+   * print_number - function that
+    * prints an integer.
+     * @n: input is an intiger
+      * Return: void
+       */
 
 void print_number(int n)
 {
-	int p = 0, m = 0, k;
-	
+	int i = 0;
+
 	/*
 	 * conditinal to cover the case in which
 	 * they give us the biggest int negative number
 	 * this condtional raise a flag -> P that we will use
 	 * in the future to print something in case this
 	 * conditional is true.
-	 * we divide this big number and remove the 8 for a while
+	 * we divide this big number and remove the 8 ifor a while
 	 * we gonna print it later at the end of the program.
-	 */
-	if (n == -2147483648)
-	{
-		m = m / 10;
-		p = 1;
-	}
+	*/
+	
 	/*
 	 * conditional that prints a character '-' to,
 	 * represent that the number we gonna print is negative
@@ -33,24 +29,13 @@ void print_number(int n)
 	 */ 
 	if (n < 0)
 	{
-		_putchar('-');
 		n = -n;
+		_putchar('-');
 	}
-	m = n;
-	while (n != 0)
+	while (i < n / 10)
 	{
-		k =1;
-		while (n/10)
-		{
-			k = k * 10;
-			m = m / 10;
-		}
-		_putchar(m);
-		n = m;
-		n = n % k;
+		print_number(i);
+	i++;
 	}
-	if (p == 1)
-		_putchar('8');
-	if (m == 0)
-		_putchar('0');
+	_putchar((n % 10) + '0');
 }
