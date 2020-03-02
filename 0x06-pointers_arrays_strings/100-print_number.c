ed290aed678751b98d63673347b568d38506670e
julianfrancor <julianfrancor@gmail.com>
2020-03-02 22:16:26 +0000

    function that prints an integer.

--- 0x06-pointers_arrays_strings/100-print_number.c
+++ 0x06-pointers_arrays_strings/100-print_number.c
@@ -9,33 +9,32 @@
 
 void print_number(int n)
 {
-	int i = 0;
+	unsigned int m;
+	int i, k;
 
-	/*
-	 * conditinal to cover the case in which
-	 * they give us the biggest int negative number
-	 * this condtional raise a flag -> P that we will use
-	 * in the future to print something in case this
-	 * conditional is true.
-	 * we divide this big number and remove the 8 ifor a while
-	 * we gonna print it later at the end of the program.
-	*/
-	
-	/*
-	 * conditional that prints a character '-' to,
-	 * represent that the number we gonna print is negative
-	 * and then, changes the sign of the number, to work easier
-	 * with it, and it will help us because we can only print with putchar
-	 */ 
 	if (n < 0)
 	{
-		n = -n;
+		n *= -1;
 		_putchar('-');
 	}
-	while (i < n / 10)
+	m = n;
+	k = 0;
+	while (m / 10 > 0)
 	{
-		print_number(i);
-	i++;
+		m /= 10;
+		k++;
 	}
-	_putchar((n % 10) + '0');
+	m = n;
+	while (k != 0)
+	{
+		for (i = 0; i < k; i++)
+		{
+			m /= 10;
+		}
+		m %= 10;
+		_putchar(m + '0');
+		k--;
+		m = n;
+	}
+	_putchar(m % 10 + '0');
 }
