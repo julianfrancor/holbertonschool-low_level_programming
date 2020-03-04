dee3fb2c04ad963b9256d68ea250736cfcf04c83
julianfrancor <julianfrancor@gmail.com>
2020-03-04 00:53:00 +0000

    function that concatenates all the arguments of your program.

---
+++ 0x0B-malloc_free/5-argstostr.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - function that
+ * concatenates all the arguments of your program.
+ * @ac: int
+ * @av: array char
+ * Return: char
+ */
+
+char *argstostr(int ac, char **av)
+{
+
