8520b167f0e9031ed488573b78c819bdff3d6507
julianfrancor <julianfrancor@gmail.com>
2020-03-09 23:53:54 +0000

    function that frees dogs.

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -3,15 +3,15 @@
 #include <stdio.h>
 
 /**
- * print_dog - function that
- * prints a struct dog
- * @d: pointer pointing to the struct dog
- * Return - Void
+ * free_dog - function that
+ * frees the dogs
+ * @d: pointer type dog_t
+ * Return: Void
  */
 
 void free_dog(dog_t *d)
 {
-	free ((*d).name);
-	free ((*d).owner);
-	free (d);
+	free((*d).name);
+	free((*d).owner);
+	free(d);
 }
