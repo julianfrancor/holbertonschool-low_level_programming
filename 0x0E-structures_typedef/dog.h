c6687f2c0ff9c92472f4ff9af3854f0459b3e881
julianfrancor <julianfrancor@gmail.com>
2020-03-09 20:58:36 +0000

    new type struct dog with the following elements

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - structure to define elements
+ * @name: is a pointer of type char
+ * @age: is a variable float
+ * @owner: is a pointer char
+ */
+
 struct dog
 {
-char *name;
-float age;
-char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
-
 #endif
