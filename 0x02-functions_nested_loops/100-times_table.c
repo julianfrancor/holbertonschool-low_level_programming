ea114db9005710f0ec21a4862cc9ea9a05f098a0
julianfrancor <julianfrancor@gmail.com>
2020-03-10 22:46:02 +0000

    function that prints the n times table, starting with 0.

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,53 +2,43 @@
 /**
  * print_times_table - function that
  * prints the n times table, starting with 0.
+ * @n: intput type int
  * Return: void
  */
+
 void print_times_table(int n)
 {
-	int i, j, mult = 1;
-	char w, f, k, q;
-	
-	if (!(n < 0 || n > 15))
+	int i, j, mult;
+
+	mult = 1;
+	if (!(n > 15))
 	{
-		for (i = 0; i <= n; i++)i
+		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
-				mult = i * j; 
-				if ((mult / 10) == 0)
+				mult = i * j;
+				if (mult > 9)
+				{
+					_putchar(',');
+					_putchar(' ');
+					if (mult < 100)
+						_putchar(' ');
+					if (mult > 99)
+						_putchar((char)(mult / 100) + '0');
+					_putchar((char)((mult / 10) % 10) + '0');
+					_putchar((char)(mult % 10) + '0');
+				}
+				else if (mult <= 9)
 				{
 					if (j > 0)
-						_putchar(' ');
-					if (!(j == 0))
-						_putchar(' ');
-					_putchar((mult % 10) + '0');
-					if (!(j == n))
-					{
-						_putchar(',');
-						if ((i <= n))
-							_putchar(' ');
-					}
-				}
-				else
-				{
-					if ((i * j) > 99)
-					{
-						_putchar((mult / 100) + '0');
-						_putchar(((mult / 10) % 10) + '0');
-						_putchar((mult % 10) + '0');
-					}
-					else
-					{
-						_putchar(' ');
-						_putchar((mult / 10) + '0');
-						_putchar((mult % 10)+ '0');
-					}
-					if (!(j == n))
 					{
 						_putchar(',');
 						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
 					}
+					_putchar((char) mult + '0');
 				}
 			}
 			_putchar('\n');
