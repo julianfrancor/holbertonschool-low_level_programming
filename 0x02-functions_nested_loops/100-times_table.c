988166f21d4cedf00b75835462a52c0b491d2e76
julianfrancor <julianfrancor@gmail.com>
2020-03-10 01:02:29 +0000

    tablas

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,54 +1,48 @@
 #include "holberton.h"
-
 /**
  * print_times_table - function that
  * prints the n times table, starting with 0.
  * Return: void
  */
-
 void print_times_table(int n)
 {
-	int i, j;
+	int i, j, mult = 1;
 	char w, f, k, q;
 	
 	if (!(n < 0 || n > 15))
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 0; i <= n; i++)i
 		{
 			for (j = 0; j <= n; j++)
 			{
-				w = (i * j) / 100;
-				f = (i * j) / 10;
-				q = ((i * j) / 10) % 10;
-				k = (i * j) % 10;
-				if (f == 0)
+				mult = i * j; 
+				if ((mult / 10) == 0)
 				{
+					if (j > 0)
+						_putchar(' ');
 					if (!(j == 0))
-					{
-					_putchar(' ');
-					}
-					_putchar(k + '0');
+						_putchar(' ');
+					_putchar((mult % 10) + '0');
 					if (!(j == n))
 					{
-					_putchar(',');
-					_putchar(' ');
-					if ((j < 9) || (i < 9))
-						_putchar(' ');
+						_putchar(',');
+						if ((i <= n))
+							_putchar(' ');
 					}
 				}
 				else
 				{
 					if ((i * j) > 99)
 					{
-						_putchar(w + '0');
-						_putchar(q + '0');
-						_putchar(k + '0');
+						_putchar((mult / 100) + '0');
+						_putchar(((mult / 10) % 10) + '0');
+						_putchar((mult % 10) + '0');
 					}
 					else
 					{
 						_putchar(' ');
-						_putchar(f + '0');
-						_putchar(k + '0');
+						_putchar((mult / 10) + '0');
+						_putchar((mult % 10)+ '0');
 					}
 					if (!(j == n))
 					{
