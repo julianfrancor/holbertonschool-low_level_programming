a4157b1448cb168c8cd4cbf09615a8e63caed16d
julianfrancor <julianfrancor@gmail.com>
2020-03-14 17:27:37 +0000

    adding header

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -3,6 +3,9 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+
+int _putchar(char c);
+int sum_them_all(const unsigned int n, ...);
 /**
  * struct print - Struct print
  *
@@ -16,8 +19,6 @@ typedef struct print
 	void (*f)(va_list);
 } print_dog;
 
-int _putchar(char c);
-int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
