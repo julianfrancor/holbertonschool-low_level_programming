09a0409edf3e2b00878a4a01912a5c6879c5bc12
julianfrancor <julianfrancor@gmail.com>
2020-03-14 17:53:13 +0000

    fixing task one

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,9 +12,9 @@
 
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int num;
+	unsigned int i;
 	va_list valist;
-	int num = 0;
-	unsigned int i = 0;
 
 	va_start(valist, n);
 	for (i = 0; i < n; i++)
