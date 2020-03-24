1e8bb016d6478da9f4513576679f08c2907a8f06
julianfrancor <julianfrancor@gmail.com>
2020-03-24 04:33:38 +0000

    function that returns the nth node of a listint_t linked list.

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+/**
+ * free_listint2 - function that
+ * frees a list_t list.
+ * @head: is a double pointer to the head of the list
+ * Return: void
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+
+	temp = *head;
+	while (*head)
+	{
+		temp = *head;
+		*head = temp->next;
+		free(temp);
+	}
+	free(*head);
+}
