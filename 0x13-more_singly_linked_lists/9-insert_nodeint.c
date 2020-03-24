46c02378ca6a94e7384fb57abdf38603996dff32
julianfrancor <julianfrancor@gmail.com>
2020-03-24 21:10:27 +0000

    fixing task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -36,7 +36,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	while (aux1)
 	{
 		aux2 = aux1;
-		aux1 = aux1->next;
+		aux1 = aux2->next;
 		indice++;
 		if (indice == idx)
 		{
