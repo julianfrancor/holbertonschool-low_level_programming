a4853a3ef14b3396992300d9e17ced16a105baa5
julianfrancor <julianfrancor@gmail.com>
2020-03-24 23:59:34 +0000

    function that deletes the node at index index of a listint_t linked list.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -11,7 +11,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *temp, *delete;
 	unsigned int nodes = 1;
 
-	if (head == NULL)
+	if (*head == NULL)
 		return (-1);
 
 	temp = *head;
