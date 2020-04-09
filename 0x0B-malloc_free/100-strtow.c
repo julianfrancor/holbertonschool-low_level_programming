698c6a0df5a6b4e08e1fddef200b57391e41bc0f
julianfrancor <julianfrancor@gmail.com>
2020-04-09 22:30:58 +0000

    function that splits a string into words.

--- 0x0B-malloc_free/100-strtow.c
+++ 0x0B-malloc_free/100-strtow.c
@@ -8,25 +8,54 @@
  * @str: char
  * Return: char
  */
-
 char **strtow(char *str)
 {
-	int i = 0, j = 0, words = 0, palabras = 0;
+	int i = 0, j = 0, words = 0, letters = 0, k = 0, q = 0;
 	char **array;
 
-	if (str == NULL || str == "")
+	if (str == NULL || str[0] == 0)
 		return (NULL);
+/*PRIMERO: MALLOC A LOS PUNTEROS how many words we have in that full string*/
 	for (i = 0; str[i]; i++)
 	{
-		if (str[i] != ' ')
+		/*counting the number of words in the str*/
+		if (str[i] != ' ' && (str[i + 1] == ' ' || str[i + 1] == '\0'))
 			words++;
-		printf("%d", words);
-		if (str[i] != ' ' && str[i+1] == ' ')
-			palabras++;
 	}
-		printf("%d'\n'", palabras);
-		printf("%d'\n'", words);
-	array = malloc(sizeof(char*) * words + palabras + 1); 
+	if (words == 0)
+		return (0);
+	/*Allocating space for the words we have and one space null at the end*/
+	array = malloc(sizeof(char *) * words + 1);
+	if (!array)
+		return (0);
+/*SEGUNDO: MALLOC A LETRAS EN DE CADA PUNTERO, letters we have on each word*/
+	for (i = 0; str[i] && k < words; i++)
+	{
+		if (str[i] != ' ')
+		{
+			/*finding the space for each string*/
+			j = i;
+			letters = 0;
+			while (str[j] != ' ' && str[j] != '\0')
+			{
+				letters++;
+				j++;
+			}
+			/*Allocating space for the number of letter within each word*/
+			array[k] = malloc(sizeof(char) * (letters + 1));
+			if (!array[k])
+			{
+				for (k = k - 1; k >= 0; k++)
+					free(array[k]);
+				free(array);
+				return (0);
+			}
+			/*initializing the matriz with str.*/
+			for (q = 0; q < letters; q++, i++)
+				array[k][q] = str[i];
+			array[k++][q] = '\0';
+		}
+	}
+	array[k] = '\0';
 	return (array);
 }
-
